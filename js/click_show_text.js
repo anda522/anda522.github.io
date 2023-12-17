2ae15015cfd1fe5fda984b54210c5daafa09e0db
anda522 <wyq522208@qq.com>
2023-12-17 16:58:35 +0800

    Site updated: 2023-12-17 16:58:33

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,7 +1,7 @@
 var a_idx = 0;
 $(document).ready(function() {
     $("body").click(function(e) {
-        var a = new Array("SegmentTree", "Trie", "LCT", "FFT", "DP", "ST", "Tarjan", "DSU", "Fenwick", "STL", "Geometry", "SAM");
+        var a = new Array("Peace", "Love", "Wisdom", "Serenity", "Hope", "Joy", "Harmony");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX, y = e.pageY;
